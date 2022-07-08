7a0303911b80c9f43d23fa86b74550a9b5df85ab
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2022-07-08 20:14:13 +0000

    Added helper function to Etherscan liveware component

--- app/Http/Livewire/Transactions/Tables/Etherscan.php
+++ app/Http/Livewire/Transactions/Tables/Etherscan.php
@@ -3,33 +3,101 @@
 namespace App\Http\Livewire\Transactions\Tables;
 
 use App\Models\ERC20;
+use App\Models\Wallet;
+use GuzzleHttp\Exception\ServerException;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Livewire\Component;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use InvalidArgumentException;
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
 
 class Etherscan extends Component
 {
     private const PER_PAGE = 20; // Records per page
 
-    public string $wallet;
-    public EloquentCollection $transactions;
+    public string $wallet; // Wallet address
+    public EloquentCollection $transactions; // Fetched transactions
 
-    public function mount(string $wallet): void
-    {
-        $this->wallet = $wallet;
-        $this->getTransactions();
-    }
+    // Query string synchronized with internal component state
+    protected array $queryString = ['wallet'];
 
     public function render()
     {
         return view('livewire.transactions.tables.etherscan');
     }
 
-    private function getTransactions()
+    public function loadTransactions(): void
     {
-        $transactions = $this->saveTransactions($this->getTransactionsFromAPI());
+        // First load transactions from database
+        $records = $this->getTransactionsQuery()->get();
+
+        // If no transactions exists against this wallet then load from API and
+        // cache them locally
+        if ($records->empty() || $records->count() == 0) {
+            $records = $this->processTransactions(
+                $this->getTransactionsFromAPI()
+            );
+
+            $this->transactions = $records->get('transactions');
+            return;
+        }
+
+        $this->transactions = $records;
+    }
+
+    public function loadMoreTransactions()
+    {
+    }
+
+    /**
+     * Checks if wallet record exists for stored wallet ID or not.
+     *
+     * @return bool
+     */
+    private function walletExists(): bool
+    {
+        return Wallet::where('wallet_id', $this->wallet)->count() > 0;
+    }
+
+    /**
+     * Parses collection of raw ERC20 transactions (fetched from API) using
+     * `parseTransaction` method, saves them using `saveTransactions` method
+     * and returns the result, a collection containing saved records, unique
+     * and existing records count.
+     *
+     * @param \Illuminate\Support\Collection<array> $transactions
+     *
+     * @return \Illuminate\Support\Collection<mixed>
+     */
+    private function processTransactions(Collection $transactions): Collection
+    {
+        return $this->saveTransactions(
+            $transactions->map(function ($transaction) {
+                return $this->parseTransaction($transaction);
+            })
+        );
+    }
+
+    /**
+     * Returns Eloquent Query Builder instance with all conditions applied for
+     * wallet address.
+     *
+     * @param ?int $limit
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    private function getTransactionsQuery(?int $limit = self::PER_PAGE): EloquentBuilder
+    {
+        return ERC20::query()
+            ->where(function (EloquentBuilder $query) {
+                return $query
+                    ->where('accounts->from', $this->wallet)
+                    ->orWhere('accounts->to', $this->wallet);
+            })
+            ->limit($limit);
     }
 
     /**
@@ -43,16 +111,25 @@ class Etherscan extends Component
      */
     private function getTransactionsFromAPI(?int $start = null): null|Collection
     {
+        // Throw an exception if invalid wallet ID was provided
+        if (!$this->wallet || !preg_match('/^0x[a-fA-F0-9]{40}$/', $this->wallet))
+            throw new InvalidArgumentException('Invalid wallet ID provided!');
+
+        // Fetch wallet transactions from API
         $response = Http::retry(3, 300)
             ->acceptJson()
             ->get('https://api.etherscan.io/api', [
-                'address' => $this->wallet,
-                'offset'  => 10000,
+                'address'    => $this->wallet,
+                'offset'     => 10000,
                 'startblock' => $start,
-                'apikey'  => config('hawk.etherscan.api_key')
+                'apikey'     => config('hawk.etherscan.api_key')
             ]);
 
-        return $response->serverError() ? null : collect($response->json()['result']);
+        // Throw new 500 error if API sent error or unexpected response
+        if ($response->serverError())
+            throw new InternalErrorException('Could not fetch transactions!');
+
+        return collect($response->json()['result']);
     }
 
 
@@ -100,15 +177,15 @@ class Etherscan extends Component
 
     /**
      * Saves passed formatted ERC20 transactions into database neglecting
-     * already existing records. Returns an array containing unique records
+     * already existing records. Returns a collection containing unique records
      * count, existing records count and collection of all passed transactions
      * eloquent models fetched/saved in database.
      *
      * @param \Illuminate\Support\Collection<array> $transactions
      *
-     * @return array
+     * @return \Illuminate\Support\Collection<mixed>
      */
-    private function saveTransactions(Collection $transactions): array
+    private function saveTransactions(Collection $transactions): Collection
     {
         // Check if any of passed transactions already exist in database or not
         $existing_transactions = ERC20::whereIn(
@@ -121,11 +198,11 @@ class Etherscan extends Component
 
         // If no record exists then save and return them
         if (!$existing_transactions || $existing_transactions->count() === 0)
-            return [
+            return new Collection([
                 'uniques'      => count($transactions),
                 'existing'     => 0,
                 'transactions' => $transactions->map(fn ($transaction) => ERC20::create($transaction)),
-            ];
+            ]);
 
         // Grab transaction hash from existing records (for filtering)
         $existing_hashes = $existing_transactions
@@ -135,17 +212,17 @@ class Etherscan extends Component
             ->filter(fn ($transaction) => $existing_hashes->contains($transaction['hash']));
 
         // All records already exist in database, return those
-        if (!$uniques || $uniques->empty()) return [
+        if (!$uniques || $uniques->empty()) return new Collection([
             'events'   => $existing_transactions,
             'existing' => $existing_transactions->count(),
             'uniques'  => 0,
-        ];
+        ]);
 
         // No record exists locally and all passed records are unique
-        return [
+        return new Collection([
             'events'   => array_merge($existing_transactions, ERC20::create($uniques)),
             'existing' => 0,
             'uniques'  => $existing_transactions->count(),
-        ];
+        ]);
     }
 }
