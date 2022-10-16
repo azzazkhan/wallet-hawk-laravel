978cf1e4a39bbab2dd07296f6c900c224df0c374
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2022-10-16 15:25:10 +0000

    Fixed incorrect button type on filter modal

--- resources/views/livewire/opensea-transactions-view.blade.php
+++ resources/views/livewire/opensea-transactions-view.blade.php
@@ -179,12 +179,14 @@
         <!-- Filter apply and clear buttons -->
         <div class="flex mt-6 space-x-2">
             @if ($filtered)
-                <x-flowbite.modal.cancel
+                <button
+                    type="button"
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:outline-none rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600"
                     wire:loading.class="cursor-wait !opacity-60"
                     wire:click="clear_filters"
                 >
                     Clear
-                </x-flowbite.modal.cancel>
+                </button>
             @endif
 
             <button
